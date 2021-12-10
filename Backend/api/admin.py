9d7178834459daf4a41fa00fddbdade0baa61afc
Malavan Srikumar <malavan.srikumar@zendesk.com>
2021-12-11 08:50:08 +1100

    Moved backend to different repository

--- Backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, IncomeSource, Income, Payment, Interval
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(IncomeSource)
-admin.site.register(Income)
-admin.site.register(Payment)
-admin.site.register(Interval)
